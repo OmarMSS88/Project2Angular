6665609a49d907235d1ff970a49fefb4c9f7312d
OmarMSS88 <91123631+OmarMSS88@users.noreply.github.com>
2024-08-09 14:42:59 +0200

    detailpage fix

--- MercenariesFrontend/mercenaries/src/app/offer-detail/offer-detail.component.ts
+++ MercenariesFrontend/mercenaries/src/app/offer-detail/offer-detail.component.ts
@@ -4,6 +4,9 @@ import { Offer } from '../models/offer';
 import { OfferService } from '../services/offer.service';
 import { ActivatedRoute } from '@angular/router';
 import { CustomDatePipe } from '../custom-date.pipe';
+import { BookingService } from '../services/booking.service';
+import { AuthService } from '@auth0/auth0-angular';
+import { CreateBooking } from '../dto/create-booking.dto';
 
 @Component({
   selector: 'app-offer-detail',
@@ -17,13 +20,29 @@ export class OfferDetailComponent implements OnInit {
   
   @Input() isGuest: boolean = false; // Flag to determine if the view is for a guest
 
-  constructor(private offerService: OfferService, private route: ActivatedRoute, private location: Location) { }
+  isAuthenticated: boolean = false;
+  userId: string | null = null;
+  hasActiveBooking: boolean = false;
+  
+  constructor(private offerService: OfferService, private route: ActivatedRoute, private location: Location, private bookingService: BookingService, private authService: AuthService) { }
+
 
   ngOnInit(): void {
     const offerId = this.route.snapshot.paramMap.get('id');
     if (offerId != null) {
       this.offerService.getOfferById(+offerId).subscribe(result => this.offer = result);
     }
+
+    this.authService.isAuthenticated$.subscribe(isAuthenticated => {
+      this.isAuthenticated = isAuthenticated;
+    });
+
+    this.authService.user$.subscribe(user => {
+      if (user?.sub) {
+        this.userId = user.sub;
+        this.checkActiveBooking();
+      }
+    });
   }
 
   goBack(): void {
@@ -32,6 +51,29 @@ export class OfferDetailComponent implements OnInit {
 
   bookOffer(): void {
     // Implement the logic to book the offer
+    if (this.userId) {
+      const bookingTime = new Date();
+
+      const bookingDto: CreateBooking = {
+        bookerAuthId: this.userId,
+        offerId: this.offer.id,
+        bookingTime: bookingTime,
+        completed: false
+      };
+
+      this.bookingService.createBooking(bookingDto).subscribe(() => {
+        console.log('Booking created successfully');
+        this.hasActiveBooking = true;
+      });
+    }
     console.log('Offer booked!');
+  }
+
+  checkActiveBooking() {
+    if (this.userId) {
+      this.bookingService.getBookingsByUser(this.userId).subscribe(bookings => {
+        this.hasActiveBooking = bookings.some(booking => booking.offerId === this.offer.id && !booking.complete);
+      });
+    }
   }
 }
