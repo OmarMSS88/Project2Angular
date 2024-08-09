4b28af0459d2f6f6794f79a68c36060421161d04
OmarMSS88 <91123631+OmarMSS88@users.noreply.github.com>
2024-08-09 14:05:54 +0200

    oude frontend deleted

--- mercenaries/src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-import { Environment } from './environment.interface';
-
-export const environment: Environment = {
-  production: false,
-  AUTH0_DOMAIN: 'dev-omar.eu.auth0.com',
-  AUTH0_CLIENT_ID: 'IJrmgYbTmQRyOQfx6lenbpduRJzfOPRh',
-  redirectUri: 'https://localhost:4200/callback',
-  api_url: 'https://localhost:6587/api'
-};
